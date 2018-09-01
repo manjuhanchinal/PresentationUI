25a852af90505546bd29cf57107fe13aee0592e7
Manjunath Hanchinal <manjunathkhanchinal@gmail.com>
2018-09-01 23:10:39 +0530

    initial checkin

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PresentationUI
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{*anything}",
+                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Scripts/employeeApp.js
@@ -0,0 +1,79 @@
+﻿var app = angular.module('MyEmployeeApp', []);
+app.controller('employeejsController', ['$scope', '$http', employeejsController]);
+
+// Angularjs Controller  
+function employeejsController($scope, $http) {
+
+    // Declare variable  
+    $scope.loading = true;
+    $scope.updateShow = false;
+    $scope.addShow = true;
+
+    // Get All Employee  
+    $http.get('http://localhost:55409/employee/').success(function (data) {
+        $scope.employees = data;
+    }).error(function () {
+        $scope.error = "An Error has occured while loading posts!";
+    });
+
+    //Insert Employee  
+    $scope.add = function () {
+        $scope.loading = true;
+        this.newemployee.Name = this.newemployee.LastName + " " + this.newemployee.FirstName;
+        $http.post('http://localhost:55409/employee/', this.newemployee).success(function (data) {
+            $scope.employees = data;
+            $scope.updateShow = false;
+            $scope.addShow = true;
+            $scope.newemployee = '';
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Adding employee! " + data;
+        });
+    }
+
+    //Edit Employee  
+    $scope.edit = function () {
+        var Id = this.employee.Id;
+        $http.get('http://localhost:55409/employee/search?Id=' + Id).success(function (data) {
+            $scope.newemployee = data;
+            var name = data.Name.split(" ");
+            $scope.newemployee.LastName = name[0];
+            $scope.newemployee.FirstName = name[1];
+            $scope.updateShow = true;
+            $scope.addShow = false;
+        }).error(function () {
+            $scope.error = "An Error has occured while loading posts!";
+        });
+    }
+
+    $scope.update = function () {
+        $scope.loading = true;
+        console.log(this.newemployee);
+        this.newemployee.Name = this.newemployee.LastName + " " + this.newemployee.FirstName;
+        $http.put('http://localhost:55409/employee/', this.newemployee).success(function (data) {
+            $scope.employees = data;
+            $scope.updateShow = false;
+            $scope.addShow = true;
+            $scope.newemployee = '';
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Saving employee! " + data;
+        });
+    }
+
+    //Delete Employee  
+    $scope.delete = function () {
+        var Id = this.employee.Id;
+        $scope.loading = true;
+        $http.delete('http://localhost:55409/employee?Id=' + Id).success(function (data) {
+            $scope.employees = data;
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Saving employee! " + data;
+        });
+    }
+
+    //Cancel Employee  
+    $scope.cancel = function () {
+        $scope.updateShow = false;
+        $scope.addShow = true;
+        $scope.newemployee = '';
+    }
+}
